161776fa7bfbc75606b70cf20121345ccbf64d14
Angélica Soares <angelicade.soares@outlook.com>
2020-06-27 17:44:06 -0300

    const b - alterando ('[tp=flappy]') para ('[tp-flappy]')

--- script.js
+++ script.js
@@ -1,24 +1,26 @@
-// criar elementos
-
-function novoElemento(tagName, className) {
-    const elem = document.createElement(tagName);
-    elem.className = className;
-    return elem;
+function novoElemento(tagName, className){
+    const elem = document.createElement(tagName)
+    elem.className = className
+    return elem
 }
 
-// criar barreiras
+function Barreira(reversa = false){
+    this.elemento = novoElemento('div', 'barreira')
 
-function Barreira(reversa = false) {
-    this.elemento = novoElemento('div', 'barreira');
-
-    const borda = novoElemento('div', 'borda');
-    const corpo = novoElemento('div', 'corpo');
-
-    this.elemento.appendChild(reversa ? corpo : borda);
-    this.elemento.appendChild(reversa ? borda : corpo);
+    const borda = novoElemento('div', 'borda')
+    const corpo = novoElemento('div', 'corpo')
+    
+    this.elemento.appendChild(reversa ? corpo : borda )
+    this.elemento.appendChild(reversa ? borda : corpo )
 
     this.setAltura = altura => corpo.style.height = `${altura}px`
+
 }
+
+///const b = new Barreira(true)
+//b.setAltura(200)
+//document.querySelector('[tp-flappy]').appendChild(b.elemento)
+
 
 function ParDeBarreiras(altura, abertura, x) {
     this.elemento = novoElemento('div', 'par-de-barreiras')
@@ -30,24 +32,24 @@ function ParDeBarreiras(altura, abertura, x) {
     this.elemento.appendChild(this.inferior.elemento)
 
     this.sortearAbertura = () => {
-        const alturaSuperior = Math.random() * (altura - abertura);
-        
+        const alturaSuperior = Math.random() * (altura - abertura)
+    
         const alturaInferior = altura - abertura - alturaSuperior
-
+    
         this.superior.setAltura(alturaSuperior)
-        
+    
         this.inferior.setAltura(alturaInferior)
     }
 
     this.getX = () => parseInt(this.elemento.style.left.split('px')[0])
-
+    
     this.setX = x => this.elemento.style.left = `${x}px`
-
+    
     this.getLargura = () => this.elemento.clientWidth
 
     this.sortearAbertura()
     this.setX(x)
 }
 
-const b = new ParDeBarreiras(700, 200, 800);
-document.querySelector('[tp=flappy]').appendChild(b.elemento
+const b = new ParDeBarreiras(700, 200, 800)
+document.querySelector('[tp-flappy]').appendChild(b.elemento)
